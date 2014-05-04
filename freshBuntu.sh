d2de2b618b1aebf481ec06522794a90c3daf099b
Stokes <mstokes5@uwo.ca>
2014-05-04 17:59:40 -0400

    1st rev

--- freshBuntu.sh
+++ freshBuntu.sh
@@ -7,22 +7,22 @@ sudo apt-get upgrade
 ###############################################################################
 # install
 ###############################################################################
-sudo apt-get install google-chrome-stable # google chrome
-sudo apt-get install openssh-client # ssh client
-sudo apt-get install openssh-server # ssh server
-sudo apt-get install git # git
-sudo apt-get install filezilla # filezilla
-sudo apt-get install gimp # gimp
-sudo apt-get install vlc # vlc
-sudo apt-get install python-pip # pip
-sudo apt-get install python-dev # python dev
-sudo apt-get install wireshark # wireshark
-sudo apt-get install tree # tree
-sudo apt-get install xclip # xclip
-sudo apt-get install libpq-dev # req for postgres
-sudo apt-get install postgresql postgresql-contrib # postgres
-sudo apt-get install pgadmin3 # pgadmin
-sudo apt-get install network-manager-openvpn # for pia
+sudo apt-get -y install google-chrome-stable # google chrome
+sudo apt-get -y install openssh-client # ssh client
+sudo apt-get -y install openssh-server # ssh server
+sudo apt-get -y install git # git
+sudo apt-get -y install filezilla # filezilla
+sudo apt-get -y install gimp # gimp
+sudo apt-get -y install vlc # vlc
+sudo apt-get -y install python-pip # pip
+sudo apt-get -y install python-dev # python dev
+sudo apt-get -y install wireshark # wireshark
+sudo apt-get -y install tree # tree
+sudo apt-get -y install xclip # xclip
+sudo apt-get -y install libpq-dev # req for postgres
+sudo apt-get -y install postgresql postgresql-contrib # postgres
+sudo apt-get -y install pgadmin3 # pgadmin
+sudo apt-get -y install network-manager-openvpn # for pia
 
 sudo pip install virtualenv # virtual env
 sudo pip install virtualenvwrapper # wrapper
@@ -30,9 +30,48 @@ sudo pip install virtualenvwrapper # wrapper
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh # heroku
 
 ###############################################################################
+# makerware
+###############################################################################
+sudo apt-add-repository 'deb http://downloads.makerbot.com/makerware/ubuntu precise main'
+cd /tmp/ && wget http://downloads.makerbot.com/makerware/ubuntu/dev@makerbot.com.gpg.key
+sudo apt-key add /tmp/dev@makerbot.com.gpg.key 
+sudo apt-get update
+sudo apt-get -y install makerware
+
+
+###############################################################################
+# Architecture Specific .debs
+###############################################################################
+architecture=`uname -m`
+case "$architecture" in 
+
+#64
+x86_64) echo "64-Bit Architecture"
+        cd /tmp/ && wget https://linux.dropbox.com/packages/ubuntu/dropbox_1.6.2_amd64.deb
+        cd /tmp/ && sudo dpkg --install dropbox_1.6.2_amd64.deb
+
+        # chrome
+        cd /tmp/ && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+        cd /tmp/ && duso dpkg --install google-chrome-stable_current_amd64.deb
+        ;;
+
+#32 / default
+*)      echo "32-Bit Architecture"
+
+        # dropbox
+        cd /tmp/ && wget https://linux.dropbox.com/packages/ubuntu/dropbox_1.6.2_i386.deb
+        cd /tmp/ && sudo dpkg --install dropbox_1.6.2_i386.deb
+
+        # chrome
+        cd /tmp/ && wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb 
+        cd /tmp/ && duso dpkg --install google-chrome-stable_current_i386.deb
+        ;;
+
+esac
+
+###############################################################################
 # config
 ###############################################################################
-
 # disable ssh password login
 sudo sed -i 's/\bPasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i 's/\bRSAAuthentication yes/RSAAuthentication no/' /etc/ssh/sshd_config
@@ -50,6 +89,7 @@ echo 'export PS1="\[\e[00;31m\]\A\[\e[0m\]\[\e[00;37m\] [\w] : @\[\e[0m\]"' >> $
 # bash_aliases
 touch $HOME/.bash_aliases
 echo "alias get='sudo apt-get install'" >> $HOME/.bash_aliases
+echo "alias back = 'cd...'" >> $HOME/.bash_aliases
 
 # guest session
 sudo echo 'allow-guest=false' >> /etc/lightdm/lightdm.conf
@@ -68,8 +108,8 @@ git config --global user.email $git_email
 # privateinternetaccess
 cd /tmp/ && wget https://www.privateinternetaccess.com/installer/install_ubuntu.sh && chmod 777 ./install_ubuntu.sh && sudo ./install_ubuntu.sh
 
-
-# ssh key-gen
+# ssh key-gen for git
+# paste in github host keys
 touch $HOME/.ssh
 cd $HOME/.ssh && ssh-keygen -t rsa
 ssh-add $HOME/.ssh/id_rsa
